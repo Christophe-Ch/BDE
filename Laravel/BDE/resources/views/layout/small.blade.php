78a2f8acb7e5b6fe68af596f6775aebef5373ec4
Corentin <corentin.hangard@viacesi.fr>
2019-01-18 12:40:42 +0100

    Initialisation Layout

---
+++ Laravel/BDE/resources/views/layout/small.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BDE CESI ROUEN</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <header>
+        <div id="logo_menu">
+            <img src="" alt="Logo">
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="#">Acceuil</a></li>
+                <li><a href="#">Evenements</a></li>
+                <li><a href="#">Boite à idées</a></li>
+                <li><a href="#">Boutique</a></li>
+            </ul>
+        </nav>
+
+        <div id="profile">
+
+        </div>
+
+    </header>
+</body>
+</html>
