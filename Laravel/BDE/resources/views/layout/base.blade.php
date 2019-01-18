d67b485110e26caf33ad60714a7acc1f603e8f46
Corentin <corentin.hangard@viacesi.fr>
2019-01-18 13:12:18 +0100

    Update Css

--- Laravel/BDE/resources/views/layout/base.blade.php
+++ Laravel/BDE/resources/views/layout/base.blade.php
@@ -6,24 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>BDE CESI ROUEN</title>
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/layout.css">
 </head>
 <body>
     <header>
-        <div id="logo_menu">
-            <img src="" alt="Logo">
-        </div>
+        <div id="menu-bar">
+            <div id="logo-menu">
+                <img src="" alt="Logo">
+            </div>
 
-        <nav>
-            <ul>
-                <li><a href="#">Acceuil</a></li>
-                <li><a href="#">Evenements</a></li>
-                <li><a href="#">Boite à idées</a></li>
-                <li><a href="#">Boutique</a></li>
-            </ul>
-        </nav>
+            <nav>
+                <ul>
+                    <li><a href="#">Acceuil</a></li>
+                    <li><a href="#">Evenements</a></li>
+                    <li><a href="#">Boite à idées</a></li>
+                    <li><a href="#">Boutique</a></li>
+                </ul>
+            </nav>
 
-        <div id="profile">
-            
+            <div id="profile"></div>
         </div>
     </header>
 </body>
