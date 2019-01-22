67b850fc774c2e2c7e4b050cda92bff3f1dfd296
Louis <louis.chochoy@viacesi.fr>
2019-01-22 14:50:15 +0100

    Debut Connexion

--- api/app.js
+++ api/app.js
@@ -1,7 +1,8 @@
-const express = require('express')
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+const express = require('express');
 const mysql = require('mysql');
 
-const app = express()
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'api',
@@ -9,15 +10,49 @@ const connection = mysql.createConnection({
     database : "bde"
 });
 
-app.get('/api/', function (req, res) {
+const app = express();
 
-    connection.query('SELECT * FROM `users`', function (err, rows, fields) {
-        if (err) throw err;
-        res.send({user : rows[0]});
-    })
+var user = express.Router();
+
+app.post('/api/register', function (req, res) {
+    connection.query('')
 })
 
-app.listen(3000, function () {
+app.post('/api/connect', function (req, res) {
+    var email = req.email;
+    var password = req.password;
+    var token = bcrypt.hash(email);
+
+    console.log(email + password);
+    
+    connection.query('SELECT `password` FROM users WHERE `email` = ' + email + '\'', function (err, rows, fields) {
+        if (err) throw err;
+        console.log(rows);
+        bcrypt.compare(password, rows, (err, res) => {
+            if (res) {
+                connection.query('UPDATE users SET api_token = ' +  token  + ' WHERE email = ' + email + ' AND password = ' + rows);
+            }
+        });
+    });
+    res.send({mail: email,
+            password: password});
+})
+
+user.route('/api/user')
+.get(function(req,res){ 
+    connection.query('SELECT * FROM `users`', function (err, rows, fields) {
+        if (err) throw err;
+        res.send({user : rows,
+        method: req.method});
+    })
+})
+.post(function (res, req) {
+    
+})
+
+app.use(user);  
+
+app.listen(3000, 'localhost', function () {
   console.log('Example app listening on port 3000!')
 })
 
