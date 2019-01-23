a0ff5b3962e08d55a8c147ba66a239700eea1c0f
Corentin <corentin.hangard@viacesi.fr>
2019-01-23 09:04:48 +0100

    Fix Iphone, event, profil

---
+++ Laravel/BDE/resources/views/evenement.blade.php
@@ -0,0 +1,68 @@
+@extends('layout.base')
+
+@section('stylesheets')
+    <link rel="stylesheet" href="/css/event.css">
+@endsection
+
+@section('header')
+    <h1 class="header_title">EVENEMENT</h1>
+@endsection
+
+@section('content')
+    @component('layout.component.search-bar')
+        @slot('placeholder')
+            Rechercher un produit...
+        @endslot
+    @endcomponent
+    <div class="evenement_list">
+        @foreach ($events as $event)
+        <div class="evenement_element">
+            @component('layout.component.list-element')
+                @slot('url')
+                    /images/CESI_Corporate_Ecole_Ingenieurs.jpg
+                @endslot
+                @slot('alt')
+                    Photo event
+                @endslot
+                @slot('title')<a class="title_event" href="/event/{{$event->id}}">{{$event->nom}}</a>@endslot
+                @slot('description'){{$event->description}}@endslot
+                @slot('actions')
+                    <button class="button" id="modalBtn">S'inscrire</button>
+                    <p>{{substr($event->date, 0, 10)}} | {{$event->prix}} €</p>
+                @endslot
+            @endcomponent
+        </div>
+        @endforeach
+    </div>
+
+    @isset($eventSelec)
+        <div class="modal" id="modal" style="display: block">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdawC4Gw3WcN02RDozJqfoprI7pfhG74FfitNGRKHkxha04qoxQg" alt="Photo">
+                        <div class="content">
+                            <h2 id="modal_title">{{$eventSelec->nom}}</h2>
+                            <div class="infos">
+                                <p>{{substr($eventSelec->date, 0, 10)}}</p>
+                                <p>{{$eventSelec->prix}} €</p>
+                            </div>
+                            <p>{{$eventSelec->description}}</p>
+                            <div class="actions">
+                                <input class="button" type="button" value="S'inscrire">
+                                <p>{{$nbUser}} participants</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="galerie_photo">
+                        <div class="photo">
+                            <p>+</p>
+                        </div>
+                        @foreach ($photos as $photo)
+                            <img class="photo" src="{{$photo->url}}" alt="Photo">
+                        @endforeach
+                    </div>
+                </div>
+        </div>
+    @endisset
+    
+@endsection
---
+++ Laravel/BDE/resources/views/profil_modifier.blade.php
@@ -0,0 +1,45 @@
+@extends('layout/base')
+
+@section('stylesheets')
+    <link rel="stylesheet" href="/css/user.css">
+@endsection
+
+@section('header')
+    <h1 class="header_title">PROFIL</h1>
+@endsection
+
+@section('content')
+<div class="profil">
+    <div id="profil_avatar">
+        <img id="profil_img" src="/images/{{Auth::user()->photo}}" alt="Profil image">
+        <p id="profil_img_change"><a href="/profil/modifier/photo">Modifier</a></p>
+    </div>
+
+    <div id="profil_content">
+        <form action="/profil/modifier/{{Auth::user()->id}}" method="post">
+            @csrf
+            <div class="profil_warp">
+                <h3 class="profil_title">Nom :</h3>
+                <input class="profil_desc_input" type="text" name="name" value="{{Auth::user()->name}}">
+            </div>
+            <div class="profil_warp">
+                <h3 class="profil_title">Prenom :</h3>
+                <input class="profil_desc_input" type="text" name="prenom" value="{{Auth::user()->prenom}}">
+            </div>
+            <div class="profil_warp">
+                <h3 class="profil_title">E-mail :</h3>
+                <input class="profil_desc_input" type="email" name="email" value="{{Auth::user()->email}}">
+            </div>
+            <div class="profil_warp">
+                <h3 class="profil_title">Centre :</h3>
+                <select name="centre" class="profil_desc_input" value="{{Auth::user()->centre}}">
+                    @foreach($centres as $centre)
+                        <option value="{{$centre->id}}">{{$centre->nom}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <input class="button" type="submit" value="Modifier">
+        </form>
+    </div>
+</div>
+@endsection
