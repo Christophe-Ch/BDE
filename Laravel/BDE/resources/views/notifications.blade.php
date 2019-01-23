9889ec98348d1146480f71460a078da200d72681
Christophe Chichmanian <chris.chich@hotmail.fr>
2019-01-23 09:43:28 +0100

    Adding a message when the user doesn't have notifications

--- Laravel/BDE/resources/views/notifications.blade.php
+++ Laravel/BDE/resources/views/notifications.blade.php
@@ -2,24 +2,28 @@
 
 @section('header')
 <div class="notifications-container">
-    @foreach ($notifications as $notification)
-        @component('layout.component.notification')
-            @slot('date')
-                {{ $notification->date }}
-            @endslot
-            @slot('title')
-                {{ $notification->titre }}
-            @endslot
-            @slot('content')
-                {{ $notification->message }}
-            @endslot
-            @slot('linkUrl')
-                {{ $notification->url }}
-            @endslot
-            @slot('id')
-                {{ $notification->id }}
-            @endslot
-        @endcomponent
+    @if($notifications->count())
+        @foreach ($notifications as $notification)
+            @component('layout.component.notification')
+                @slot('date')
+                    {{ $notification->date }}
+                @endslot
+                @slot('title')
+                    {{ $notification->titre }}
+                @endslot
+                @slot('content')
+                    {{ $notification->message }}
+                @endslot
+                @slot('linkUrl')
+                    {{ $notification->url }}
+                @endslot
+                @slot('id')
+                    {{ $notification->id }}
+                @endslot
+            @endcomponent
     @endforeach
+    @else
+        <h2>Vous n'avez pas de notifications.</h2>
+    @endif
 </div>
 @endsection
