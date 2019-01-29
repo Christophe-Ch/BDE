7cd067afac46ef206b37d5e6b59126440ab3f51a
louis <louis.chochoy@viacesi.fr>
2019-01-29 23:32:33 +0100

    Fix html pour w3c validator

--- Laravel/BDE/resources/views/ideas/create.blade.php
+++ Laravel/BDE/resources/views/ideas/create.blade.php
@@ -19,9 +19,9 @@
     <form id="form-idea" action="/ideas" method="post">
         @csrf
         <label for="nom" class="title_input_left">Titre :</label>
-        <input class="input_left" type="text" name="nom">
+        <input id="nom" class="input_left" type="text" name="nom">
         <label for="description" id="description-form" class="title_input_left">Description :</label>
-        <textarea class="input_left" name="description" maxlength="255">
+        <textarea  id="description" class="input_left" name="description" maxlength="255">
         </textarea>
         <input id="form-submit" class="button btn_left" type="submit" value="Créer">
     </form>
--- Laravel/BDE/resources/views/ideas/edit.blade.php
+++ Laravel/BDE/resources/views/ideas/edit.blade.php
@@ -20,9 +20,9 @@
         @csrf
         @method('PUT')
         <label for="nom" class="title_input_left">Titre :</label>
-        <input class="input_left" type="text" name="nom" value="{{ $edit->nom }}" maxlength="40">
+        <input id="nom" class="input_left" type="text" name="nom" value="{{ $edit->nom }}" maxlength="40">
         <label for="description" class="title_input_left">Description :</label>
-        <textarea class="input_left" name="description" maxlength="255">
+        <textarea id="description" class="input_left" name="description" maxlength="255">
             {{ $edit->description}}
             </textarea>
         <input id="form-submit" class="button btn_left" type="submit">
